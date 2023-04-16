62905fdc7d1cab0e980980cb462068aba59efc9c
itsjyde <126663569+Chagbo@users.noreply.github.com>
2023-04-16 09:35:14 +0100

    First Commit

---
+++ resources/lang/en/custom-menu.php
@@ -0,0 +1,11 @@
+<?php return array (
+  'nav-menu' => 
+  array (
+    'ai-blog' => 'AI Blog',
+    'courses' => 'Courses',
+    'bundles' => 'Bundles',
+    'forums' => 'Forums',
+    'contact' => 'Contact',
+    'about-us' => 'About Us',
+  ),
+);
---
+++ script.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/usr/bin/php /home/u825857162/domains/theworkerbulls.co.uk/public_html/script/php artisan schedule:run >> /dev/null 2>&1
